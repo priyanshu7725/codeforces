304392087ad6f03f64cae0e4ad3a398b4a1ffd39
priyanshu7725 <priyanshukumar3424@gmail.com>
2024-12-14 21:10:44 +0530

    Added comments

--- watermelon.java
+++ watermelon.java
@@ -5,6 +5,10 @@ public class watermelon {
         Scanner sc = new Scanner(System.in);
         int w = sc.nextInt();
 
+        // This problem was about dividing a number into two even parts
+        // only even numbers can be divide into two even number
+        // 2 would be the only exception to this rule
+
         if ( w!=2 && w%2 == 0){
             System.out.println("YES");
         }
