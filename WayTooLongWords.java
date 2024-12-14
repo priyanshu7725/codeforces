a0f8572625e7b4519a85a39093b2d047057a3091
priyanshu7725 <priyanshukumar3424@gmail.com>
2024-12-14 21:50:45 +0530

    Problem 2

---
+++ WayTooLongWords.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class WayTooLongWords {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for ( int i = 0; i < n; i++){
+            String str = sc.next();
+            int len = str.length();
+            if ( len > 10){
+                // we add "" so that the characters are treated as strings and  concatanated 
+                // otherwise their ascii values will be taken as integers and added with (len-2)
+                System.out.println(str.charAt(0) + "" + (len-2) + str.charAt(len-1));
+            }
+            else {
+                System.out.println(str);
+            }
+        }
+        sc.close();
+    }
+}
