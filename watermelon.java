b557e036f00c8f658d3d2e0c4837b4878dc4dc57
priyanshu7725 <priyanshukumar3424@gmail.com>
2024-12-14 21:57:23 +0530

    Changed name

--- watermelon.java
+++ watermelon.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class watermelon {
+public class Watermelon {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int w = sc.nextInt();
