030ea8ae78114b74ecfe02e99f762a783e41f605
priyanshu7725 <priyanshukumar3424@gmail.com>
2024-12-19 19:08:14 +0530

    19 Dec 2024

--- BuyAShovel.java
+++ BuyAShovel.java
@@ -6,8 +6,12 @@ public class BuyAShovel {
         int k = sc.nextInt();
         int r = sc.nextInt();
 
+        // minimum 1 shovel is to be bought
         int shovel = 1;
         while(true){
+
+            // if after using 10 burle coins, the r coin needs be used
+            // or if shovel can be bought directly using 10 burle coins
             if ( (k*shovel)%10 == r || (k*shovel)%10 == 0) {
                 System.out.println(shovel);
                 break;
