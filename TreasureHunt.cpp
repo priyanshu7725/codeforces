c5613622fb315657ae1510f47d21ab5311d4f654
priyanshu7725 <priyanshukumar3424@gmail.com>
2025-04-01 22:07:28 +0530

    01-04-2025

---
+++ TreasureHunt.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while (t--) {
+        int x,y;
+        float a;
+        int depth = 0;
+        bool lilB = true;
+        cin>>x>>y>>a;
+
+        while (depth < a+0.5) {
+            if (lilB) {
+                depth+=x;
+            }
+            else {
+                depth += y;
+            }
+            if ( depth >= a+0.5 ) {
+                break;
+            }
+            else {
+                lilB = !lilB;
+            }
+        }
+
+        if ( lilB) {
+            cout<<"NO"<<endl;
+        }
+        else {
+            cout<<"YES"<<endl;
+        }
+    } 
+
+    return 0;
+}
